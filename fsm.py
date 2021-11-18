2f4fb657d84a6ef1da68b3559dde53fe2ea0d194
RayhanHamada <rayhanhamada1999@gmail.com>
2021-11-18 15:38:55 +0700

    improvisasi

--- fsm.py
+++ fsm.py
@@ -13,11 +13,18 @@ dfa2 = {
 }
 
 
-def accepts(transitions: dict[int, dict[str, int]], initial: int, accepting: list[int], s: str):
-    state = initial
-    for c in s:
-        state = transitions[state][c]
-    return "Diterima" if state in accepting else "Tidak diterima"
+def bikin_flat(l: list[list]):
+    return [item for sublist in l for item in sublist]
 
 
-print(accepts(dfa1, 0, [0, 1], 'aaaabab'))
+def accepts(transisi: dict[int, dict[str, int]], node_inisial: int, node_penerima: list[int], input_string: str):
+    node = node_inisial
+    for c in input_string:
+        if c not in bikin_flat([transisi[x].keys() for x in transisi.keys()]):
+            return f"Tidak diterima (alasan: terminal '{c}' tidak ada dalam kamus pada DFA ini)"
+
+        node = transisi[node][c]
+    return "Diterima" if node in node_penerima else f"Tidak diterima (alasan: node q{node} bukan node penerima)"
+
+
+print(accepts(dfa1, 0, [0, 1], 'aaabbaba'))
