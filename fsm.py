70d7586635fe8b8f3a6d7edbeda992f175a4fff9
RayhanHamada <rayhanhamada1999@gmail.com>
2021-11-18 15:48:38 +0700

    improve lagi

--- fsm.py
+++ fsm.py
@@ -1,18 +1,5 @@
 
 
-dfa1 = {
-    0: {'a': 0, 'b': 1},
-    1: {'a': 0, 'b': 2},
-    2: {'a': 2, 'b': 2},
-}
-
-dfa2 = {
-    0: {'a': 1},
-    1: {'b': 2},
-    2: {'a': 1}
-}
-
-
 def bikin_flat(l: list[list]):
     return [item for sublist in l for item in sublist]
 
@@ -21,10 +8,44 @@ def accepts(transisi: dict[int, dict[str, int]], node_inisial: int, node_penerim
     node = node_inisial
     for c in input_string:
         if c not in bikin_flat([transisi[x].keys() for x in transisi.keys()]):
-            return f"Tidak diterima (alasan: terminal '{c}' tidak ada dalam kamus pada DFA ini)"
+            return f"String '{input_string}' TIDAK DITERIMA (alasan: terminal '{c}' tidak ada dalam kamus pada DFA ini)"
 
         node = transisi[node][c]
-    return "Diterima" if node in node_penerima else f"Tidak diterima (alasan: node q{node} bukan node penerima)"
+    return f"String '{input_string}' DITERIMA" if node in node_penerima else f"String '{input_string}' TIDAK DITERIMA (alasan: node q{node} bukan node penerima)"
 
 
-print(accepts(dfa1, 0, [0, 1], 'aaabbaba'))
+# soal 2
+print('DFA 1')
+node_awal = 0
+node_penerima = [0, 1]
+dfa1 = {
+    0: {'a': 0, 'b': 1},
+    1: {'a': 0, 'b': 2},
+    2: {'a': 2, 'b': 2},
+}
+
+# abababaa
+print(accepts(dfa1, node_awal, node_penerima, 'abababaa'))
+
+# aaaabab
+print(accepts(dfa1, node_awal, node_penerima, 'aaaabab'))
+
+# aaabbaba
+print(accepts(dfa1, node_awal, node_penerima, 'aaabbaba'))
+
+
+# soal 3
+print('DFA 2')
+node_awal = 0
+node_penerima = [2]
+dfa2 = {
+    0: {'a': 1},
+    1: {'b': 2},
+    2: {'a': 1}
+}
+
+# abab
+print(accepts(dfa2, node_awal, node_penerima, 'abab'))
+
+# ababa
+print(accepts(dfa2, node_awal, node_penerima, 'ababa'))
